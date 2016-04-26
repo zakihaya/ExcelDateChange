9cc0112be7ca2c5d05dba4fc1e3feec0706beff5
hayazaki <toru.hayazaki@gmail.com>
2016-04-26 12:35:25 +0900

    コンテンツの作成日時変更を実装

--- ExcelDataChange/Form1.Designer.cs
+++ ExcelDataChange/Form1.Designer.cs
@@ -29,6 +29,13 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // button1
@@ -41,21 +48,99 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "保存";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ファイル";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "コンテンツの作成日時";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "前回保存日時";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(434, 19);
+            this.textBox1.TabIndex = 5;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy/MM/dd HH:mm";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(126, 82);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 19);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CustomFormat = "yyyy/MM/dd HH:mm";
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(126, 117);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 19);
+            this.dateTimePicker2.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(572, 154);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
     }
 }
 
--- ExcelDataChange/Form1.cs
+++ ExcelDataChange/Form1.cs
@@ -15,6 +15,8 @@ namespace ExcelDataChange
 {
     public partial class Form1 : Form
     {
+        private string excelName;
+
         public Form1()
         {
             InitializeComponent();
@@ -27,17 +29,18 @@ namespace ExcelDataChange
 
             if (result == DialogResult.OK)
             {
-                string excelName = ofd.FileName;
+                this.excelName = ofd.FileName;
+                this.textBox1.Text = this.excelName;
 
                 Excel.Application oXls;
                 Excel.Workbook oWBook;
 
                 oXls = new Excel.Application();
-                oXls.Visible = true;
+                oXls.Visible = false;
 
                 // Excelファイルをオープンする
                 oWBook = (Excel.Workbook)(oXls.Workbooks.Open(
-                  excelName,  // オープンするExcelファイル名
+                  this.excelName,  // オープンするExcelファイル名
                   Type.Missing, // （省略可能）UpdateLinks (0 / 1 / 2 / 3)
                   Type.Missing, // （省略可能）ReadOnly (True / False )
                   Type.Missing, // （省略可能）Format
@@ -56,22 +59,59 @@ namespace ExcelDataChange
                   Type.Missing  // （省略可能）CorruptLoad
                 ));
 
-                //Microsoft.Office.Core.DocumentProperties properties;
-                //object DocProps = oWBook.BuiltinDocumentProperties;
-                //properties = (Microsoft.Office.Core.DocumentProperties) oWBook.BuiltinDocumentProperties;
-
-                //Microsoft.Office.Core.DocumentProperty prop;
-                //prop = properties["Revision Number"];
-
                 dynamic properties = oWBook.BuiltinDocumentProperties;
-                dynamic property = properties.Item("Author").Value = "あいうえお";
+                dynamic creationDate = properties.Item("Creation date");
+                dynamic lastSaveTime = properties.Item("Last save time");
 
-                oWBook.Save();
+                this.dateTimePicker1.Value = creationDate.Value;
+                this.dateTimePicker2.Value = lastSaveTime.Value;
+
                 oWBook.Close();
                 oXls.Quit();
-
-                int i = 1;
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Excel.Application oXls;
+            Excel.Workbook oWBook;
+
+            oXls = new Excel.Application();
+            oXls.Visible = true;
+
+            // Excelファイルをオープンする
+            oWBook = (Excel.Workbook)(oXls.Workbooks.Open(
+              this.excelName,  // オープンするExcelファイル名
+              Type.Missing, // （省略可能）UpdateLinks (0 / 1 / 2 / 3)
+              Type.Missing, // （省略可能）ReadOnly (True / False )
+              Type.Missing, // （省略可能）Format
+                            // 1:タブ / 2:カンマ (,) / 3:スペース / 4:セミコロン (;)
+                            // 5:なし / 6:引数 Delimiterで指定された文字
+              Type.Missing, // （省略可能）Password
+              Type.Missing, // （省略可能）WriteResPassword
+              Type.Missing, // （省略可能）IgnoreReadOnlyRecommended
+              Type.Missing, // （省略可能）Origin
+              Type.Missing, // （省略可能）Delimiter
+              Type.Missing, // （省略可能）Editable
+              Type.Missing, // （省略可能）Notify
+              Type.Missing, // （省略可能）Converter
+              Type.Missing, // （省略可能）AddToMru
+              Type.Missing, // （省略可能）Local
+              Type.Missing  // （省略可能）CorruptLoad
+            ));
+
+            dynamic properties = oWBook.BuiltinDocumentProperties;
+            dynamic creationDate = properties.Item("Creation date");
+            dynamic lastSaveTime = properties.Item("Last save time");
+
+            creationDate.Value = this.dateTimePicker1.Value.ToString("yyyy/MM/dd HH:mm:ss");
+            lastSaveTime.Value = this.dateTimePicker2.Value.ToString("yyyy/MM/dd HH:mm:ss");
+
+            oWBook.Save();
+            oWBook.Close();
+            oXls.Quit();
+
+            int i = 1;
         }
     }
 }
